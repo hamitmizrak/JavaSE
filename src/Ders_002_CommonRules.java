d782274f843357a73d8fd2783885bac0f95e5ae0
Hamit Mizrak <hamitmizrak@gmail.com>
2020-08-11 22:20:34 +0300

    Java Common Rules

---
+++ src/Ders_002_CommonRules.java
@@ -0,0 +1,18 @@
+
+/*
+ 
+Packet isimleri küçük yazmak (com.java.product)
+Packet ismi: com.org.deneme com.sirketadi.deneme
+İmport packetadi.classadi
+
+Class isimleri : MyClass (Pascal Case) 
+Değişken adları  degiskenAdi(Camel Case)
+Java case sensitive bir dildir (Büyük küçük harf duyarlılığı vardır)
+
+
+
+
+ */
+public class Ders_002_CommonRules {
+
+}
